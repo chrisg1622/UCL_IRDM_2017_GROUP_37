ca6ea487b50cacc3c25de813f0642450ea69c1ed
chrisg1622 <chrisg1622@gmail.com>
2017-03-21 14:36:52 +0000

    .

--- metrics.py
+++ metrics.py
@@ -1,21 +1,24 @@
+from sklearn import metrics
+from numpy import sqrt
+
 def metrics_regress(predicts,ground_truth):
     #Explained variance score
-    exp_var = metrics.explained_variance_score(predicts,ground_truth) # 0 to 1, the higher the better
+    exp_var = metrics.explained_variance_score(ground_truth,predicts) # 0 to 1, the higher the better
     #Mean Absolute Error
-    mean_abs_err = metrics.mean_absolute_error(predicts,ground_truth) # the lower the better
+    mean_abs_err = metrics.mean_absolute_error(ground_truth,predicts) # the lower the better
     #Mean Squared Error
-    mean_sq_err = metrics.mean_squared_error(predicts,ground_truth)
+    mean_sq_err = metrics.mean_squared_error(ground_truth,predicts)
     #Root Mean Squared Error
-    rms = sqrt(mean_sq_err)
+    rmse = sqrt(mean_sq_err)
     #R2 coefficient
-    r2_sc = metrics.r2_score(predicts,ground_truth)
+    r2_sc = metrics.r2_score(ground_truth,predicts)
     
-    return exp_var,mean_abs_err,mean_sq_err,rms,r2_sc
+    return exp_var,mean_abs_err,mean_sq_err,rmse,r2_sc
     
     
 
 def evaluate(predicts,ground_truth):
-    exp_var,mean_abs_err,mean_sq_err,rms,r2_sc = metrics_regress(predicts,ground_truth)
+    exp_var,mean_abs_err,mean_sq_err,rms,r2_sc = metrics_regress(ground_truth,predicts)
     print("Explained variance score: {}".format(exp_var))
     print("Mean Absolute Error: {}".format(mean_abs_err))
     print("Root Mean Square Error: {}".format(rms))
