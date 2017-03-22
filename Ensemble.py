c848407a1bb984625b2310dd49fffd5e24e07885
chrisg1622 <chrisg1622@gmail.com>
2017-03-22 19:33:56 +0000

    Added Ensemble Model

--- Ensemble.py
+++ Ensemble.py
@@ -13,6 +13,10 @@ from sklearn.model_selection import KFold
 from sklearn.linear_model import LinearRegression
 import metrics as m
 
+#function to cap predictions on a scale of 1 to 3
+def post_process_preds(y_pred):
+    return np.array(max(min(y_pred,3.0),1.0))
+
 models=['LinearRegression',
           'Lasso',
           'Ridge',
@@ -24,12 +28,45 @@ models=['LinearRegression',
 
 CV_results = {}
 predictions = {}
+#read in the predictions and CV rmse means for each model
 print('Attempting to load CV Results...')
 for model in models:
     print('Reading {} CV Results...'.format(model))
     CVResults_df = pd.read_csv(str('output/'+model+'CVResults.csv'),index_col='Unnamed: 0')
     preds_df = pd.read_csv(str('output/'+model+'Predictions.csv'),index_col='id')
+    preds_df.columns = ['relevance']
     CV_results[model] = CVResults_df.loc['rmse','mean']
     predictions[model] = preds_df
+
+#%%
+
+#compute weights
+weights = pd.DataFrame()
+min_rmse = min(CV_results.values())
+for model in models:
+    weights.loc[model,'Weight'] = CV_results[model]
     
+weights.Weight = weights.Weight.apply(lambda x: 1.0 - 15*(x - min_rmse))
+norm = sum(weights.Weight)
+weights.Weight = weights.Weight.apply(lambda x: x/norm)
+
+#%%
+print('Combining model predictions')
+#predict ensemble predictions
+ensemble_predictions = pd.DataFrame(index = predictions['LinearRegression'].index, columns = ['relevance'])
+for i,model in enumerate(models):
+    print(' Processing {} model'.format(model))
+    weight = weights.loc[model,'Weight']
+    if i == 0:
+        ensemble_predictions.relevance = predictions[model].relevance.apply(lambda x: x *  weight)
+    else:
+        ensemble_predictions.relevance = ensemble_predictions.relevance + predictions[model].relevance.apply(lambda x: x * weight)
+
+ensemble_predictions.relevance = ensemble_predictions.relevance.apply(lambda x: post_process_preds(x))
     
+#%%
+#check model predictions ( min & max )
+for model in models:
+    print('Model: {} \t Min: {} \t Max: {}'.format(model,min(predictions[model]['relevance']),max(predictions[model]['relevance'])))
+
+print('Model: Ensemble \t Min: {} \t Max: {}'.format(min(ensemble_predictions['relevance']),max(ensemble_predictions['relevance'])))
